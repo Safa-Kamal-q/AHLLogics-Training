c90a357f5c6d77380e4cffcc0d6d368a70986909
Safa <safa.qasrawi.k@gmail.com>
2024-10-11 15:56:13 +0300

    write home page with footer and navbar

--- FinalProject/BookHeaven/client/src/App.jsx
+++ FinalProject/BookHeaven/client/src/App.jsx
@@ -1,9 +1,19 @@
-function App() {
-  const [count, setCount] = useState(0)
+import React from 'react'
+import Home from './pages/Home'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+function App() {
   return (
     <>
-      
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
   )
 }
