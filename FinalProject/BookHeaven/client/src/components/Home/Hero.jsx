92144b9f8421f200a146ec795ed632ebfc07d67d
Safa <safa.qasrawi.k@gmail.com>
2024-10-14 00:23:50 +0300

    make the navbar reponsive

--- FinalProject/BookHeaven/client/src/components/Home/Hero.jsx
+++ FinalProject/BookHeaven/client/src/components/Home/Hero.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] flex">
-      <div className="w-full lg:w-3/6 flex flex-col items-center lg:items-start justify-center">
+    <div className="h-screen md:h-[75vh] flex flex-col md:flex-row item-center justify-center">
+      <div className="w-full mb-12 md:mb-0 lg:w-3/6 flex flex-col items-center lg:items-start justify-center">
         <h1 className="text-4xl lg:text-6xl font-semibold text-yellow-100 text-center lg:text-left">
           Find Yourself in a Great Book
         </h1>
@@ -14,9 +15,12 @@ const Hero = () => {
           read today, and let the journey begin!
         </p>
         <div className="mt-8">
-          <button className="text-yellow-100 text-xl lg:text-2xl font-semibold border border-yellow-100 px-10 py-3 hover:bg-zinc-800 rounded-full">
+          <Link
+            to="/all-books"
+            className="text-yellow-100 text-xl lg:text-2xl font-semibold border border-yellow-100 px-10 py-3 hover:bg-zinc-800 rounded-full"
+          >
             Discover Books
-          </button>
+          </Link>
         </div>
       </div>
       <div className="w-full lg:w-3/6 h-auto lg:h-[100%] flex items-center justify-center ">
--- FinalProject/BookHeaven/client/src/components/Navbar/Navbar.jsx
+++ FinalProject/BookHeaven/client/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import { useState } from "react"
+import { FaGripLines } from "react-icons/fa";
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
   const links = [
@@ -17,31 +19,96 @@ const Navbar = () => {
     },
   ];
 
+  const [MobileNav, setMobileNav] = useState("hidden");
+
   return (
-    <div className="flex bg-zinc-800 text-white px-8 py-4 item-center justify-between">
-      <Link to="/" className="flex item-center">
-        <img
-          className="h-10 me-3"
-          src="../public/img/logo.png"
-          alt="logo"
-        />
-        <h1 className="text-2xl font-semibold text-wight py-1">BookHeaven</h1>
-      </Link>
-      <div className="nav-links-bookheaven flex item-center gap-4">
-        <div className="flex gap-1">
-          {links.map((items, i) => (
-            <Link to={items.link}
-            className="hover:text-yellow-100 transition-all duration-300 px-2 py-2" key={i}>
-              {items.title}
+    <>
+      <nav className="z-50 relative flex bg-zinc-800 text-white px-8 py-4 item-center justify-between">
+        <Link to="/" className="flex item-center">
+          <img
+            className="h-10 me-3"
+            src="../public/img/logo.png"
+            alt="logo"
+          />
+          <h1 className="text-2xl font-semibold text-wight py-1">BookHeaven</h1>
+        </Link>
+        <div className="nav-links-bookheaven block md:flex item-center gap-4">
+          <div className="hidden md:flex gap-1">
+            {links.map((items, i) => (
+              <Link
+                to={items.link}
+                className="hover:text-yellow-100 transition-all duration-300 px-2 py-2"
+                key={i}
+              >
+                {items.title}
+              </Link>
+            ))}
+          </div>
+          <div className="hidden md:flex gap-4">
+            <Link
+              to="/LogIn"
+              className="px-4 py-1 border border-wight rounded hover:bg-zinc-300 transition-all duration-300"
+            >
+              LogIn
             </Link>
-          ))}
+            <Link
+              to="/SignUp"
+              className="px-4 py-1 bg-yellow-100 rounded hover:bg-zinc-300 text-zinc-900 transition-all duration-300"
+            >
+              SignUp
+            </Link>
+          </div>
+          <button
+            className="block md:hidden text-wight text-2xl hover:text-zinc-400"
+            onClick={() =>
+              MobileNav === "hidden"
+                ? setMobileNav("block")
+                : setMobileNav("hidden")
+            }
+          >
+            <FaGripLines />
+          </button>
         </div>
-        <div className="flex gap-4">
-          <Link to="/LogIn"  className="px-4 py-1 border border-wight rounded hover:bg-zinc-300 transition-all duration-300">logIn</Link>
-          <Link to="/SignUp" className="px-4 py-1 bg-yellow-100 rounded hover:bg-zinc-300 text-zinc-900 transition-all duration-300">SignUp</Link>
-        </div>
+      </nav>
+      <div className={`${MobileNav} bg-zinc-800 h-screen absolute top-0 left-0 w-full z-40 flex flex-col items-center justify-center`}>
+        {links.map((items, i) => (
+          <Link
+            to={items.link}
+            className={`${MobileNav} text-white text-4xl mb-8 font-semibold hover:text-yellow-100 transition-all duration-300`}
+            key={i}
+            onClick={() =>
+              MobileNav === "hidden"
+                ? setMobileNav("block")
+                : setMobileNav("hidden")
+            }
+          >
+            {items.title}
+          </Link>
+        ))}
+        <Link
+          to="/LogIn"
+          className={`${MobileNav} px-8 mb-8 text-3xl font-semibold  py-2 border border-wight rounded text-white hover:bg-zinc-300 transition-all duration-300`}
+          onClick={() =>
+            MobileNav === "hidden"
+              ? setMobileNav("block")
+              : setMobileNav("hidden")
+          }
+        >
+          LogIn
+        </Link>
+        <Link
+          to="/SignUp"
+          className={`${MobileNav} px-8 mb-8 text-3xl font-semibold py-2 bg-yellow-100 rounded hover:bg-zinc-300 text-zinc-900 transition-all duration-300`}
+          onClick={() =>
+            MobileNav === "hidden"
+              ? setMobileNav("block")
+              : setMobileNav("hidden")
+          }
+        >
+          SignUp
+        </Link>
       </div>
-    </div>
+    </>
   );
 };
 
